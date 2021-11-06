8b2e0d11aee08273c1c1386f8a56a75add2956fa
IlyaVolchenko <volchenko.ilya7@gmail.com>
2021-11-06 20:49:28 +0800

    movies

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-console.log('arr' + ' - object');
-console.log(4 + '5');
+const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let user = 'ilya';
+const personalMovieDB = {
+   count: number0fFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
